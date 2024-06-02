d5b0dfbf71f18e753f633f8a50bfc8665f8705be
SasikumarDeveloper <sasikumarponnekanti23@gmail.com>
2024-06-02 18:06:36 -0400

    first commit

---
+++ Blazor_Wasm/wwwroot/callcs.js
@@ -0,0 +1,24 @@
+﻿console.log('CS to JS File');
+ 
+
+
+window.addMethod = () => {
+    DotNet.invokeMethodAsync('Blazor_Wasm', 'Addition',10,20)
+        .then(data => {
+            alert(data);
+        });
+};
+
+window.display = () => {
+    DotNet.invokeMethodAsync('Blazor_Wasm', 'ShowMessage')
+        .then(data => {
+            alert(data);
+        });
+};
+
+window.getData = () => {
+    DotNet.invokeMethodAsync('Blazor_Wasm', 'GetDepartments')
+        .then(data => {
+            alert(data);
+        });
+};
